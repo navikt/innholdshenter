fa4b5f979bb889f2c9bfbb2b987e6421961d0fab
Eivind Siqveland Larsen <Eivind.Siqveland.Larsen@nav.no>
2013-07-05 10:28:06 +0000

    RandomizeURLTest: test av bygging av tilfeldig url for henting av nytt innhold. pom: dependencies for mock av statiske klasser.
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/nav-enonic-cms-innholdshenter/trunk@43842 6ec884bf-e345-0410-bd42-d282eb737ba0

---
+++ src/test/java/no/nav/innholdshenter/common/RandomizeURLTest.java
@@ -0,0 +1,63 @@
+package no.nav.innholdshenter.common;
+
+import junit.framework.Assert;
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.validator.routines.UrlValidator;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers.*;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Matchers.anyInt;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+import static org.mockito.Mockito.verify;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(RandomStringUtils.class)
+public class RandomizeURLTest {
+    public static final String RANDOMSTRING = "AAAb312RAWEFF";
+    EnonicContentRetriever enonicContentRetriever;
+
+    private static final String cacheName = "TestEhcacheCacheName";
+    private static final String SERVER = "http://www-t1.nav.no:9000/";
+    private static final String PATH = "systemsider/ApplicationFrame";
+    private static final String URLPARAMS = "?1&fdsafad=321&fine=cool";
+
+    @Before
+    public void setUp() {
+        enonicContentRetriever = new EnonicContentRetriever(cacheName);
+        
+    }
+    
+    @Test
+    public void testGenerateRandomURL() {
+        mockStatic(RandomStringUtils.class);
+
+        when(RandomStringUtils.randomAlphanumeric(anyInt())).thenReturn(RANDOMSTRING);
+        String url = SERVER + PATH;
+        String randomUrl = enonicContentRetriever.makeRandomUrl(url);
+
+        UrlValidator validator = new UrlValidator();
+        assertTrue(validator.isValid(randomUrl));
+        assertEquals(url + "?sid=" + RANDOMSTRING, randomUrl);
+    }
+
+    @Test
+    public void testGenerateRandomURLWithLeadingParams() {
+        mockStatic(RandomStringUtils.class);
+
+        when(RandomStringUtils.randomAlphanumeric(anyInt())).thenReturn(RANDOMSTRING);
+        String url = SERVER + PATH + URLPARAMS;
+        String randomUrl = enonicContentRetriever.makeRandomUrl(url);
+
+        assertTrue(randomUrl.contains("sid=" + RANDOMSTRING));
+        UrlValidator validator = new UrlValidator();
+        assertTrue(validator.isValid(randomUrl));
+    }
+}
